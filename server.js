2f1fdaf6b02d3507761c916ada0c990692a5ea2a
alfonso <alfo8531@gmail.com>
2026-04-03 18:13:02 -0700

    Fix Express 5 RegExp error in catch-all routing

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ app.post('/api/stories', async (req, res) => {
 });
 
 // Fallback to index.html for SPA routing
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
